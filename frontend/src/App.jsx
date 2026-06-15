323afbf4e0ad4bf7823b9f28ef2234013bb93fde
Gaffiu <97696337+Gaffiu@users.noreply.github.com>
2026-06-15 20:09:04 -0300

    Add Upload and Watch routes to App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Upload from './pages/Upload';
+import Watch from './pages/Watch';
 
 export default function App() {
   return (
@@ -10,6 +12,8 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/upload" element={<Upload />} />
+        <Route path="/watch/:id" element={<Watch />} />
       </Routes>
     </BrowserRouter>
   );
